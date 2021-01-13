e6d8c4a5755300c17e29968358b77c99083e1220
Ioan Simiciuc <nioasca@gmail.com>
2021-01-13 18:39:41 +0200

    Added for Bad Request for Number Format Exception

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -41,7 +41,7 @@ public class RecipeServiceImpl implements RecipeService{
     public Recipe findById(long l) {
         Optional<Recipe> recipeOptional = recipeRepository.findById(l);
         if(!recipeOptional.isPresent())
-            throw new NotFoundException("Recipe Not Found");
+            throw new NotFoundException("Recipe Not Found, For ID value: "+l);
         return  recipeOptional.get();
     }
 
